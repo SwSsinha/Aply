63de0bae969a0793d8de9adf4c9323a049db8cc0
Swastik Sinha <sws23sinha4217@gmail.com>
2025-09-04 21:41:59 +0000

    Fix require path for resumeController in api.js

--- aply-backend/src/routes/api.js
+++ aply-backend/src/routes/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const resumeController = require('./controllers/resumeController');
+const resumeController = require('../controllers/resumeController');
 
 const router = express.Router();
 
